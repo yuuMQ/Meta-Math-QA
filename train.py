0c26f6f3ae0aa8639b29aa90a612ca81cf371f7f
Minh Quân Phạm <minhquan7ahaha@gmail.com>
2026-06-05 17:56:53 +0700

    update train.py

--- train.py
+++ train.py
@@ -1,5 +1,13 @@
+import warnings
 import torch
 import os
+warnings.filterwarnings(
+    "ignore",
+    category=FutureWarning,
+    module="timm"
+)
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
 from datetime import datetime
 import json
 
@@ -14,7 +22,7 @@ from transformers import (
     TrainerState,
     set_seed,
     BitsAndBytesConfig,
-    pipeline,
+    pipeline, AutoModelForMaskedLM,
 )
 from peft import (
     LoraConfig,
@@ -23,7 +31,7 @@ from peft import (
     prepare_model_for_kbit_training,
     PeftModel
 )
-from trl import SFTTrainer
+from trl import SFTTrainer, SFTConfig
 import bitsandbytes as bnb
 from langchain_core.prompts import (
     PromptTemplate,
@@ -47,6 +55,13 @@ from preprocessing import (
     SYSTEM_PROMPT_VI,
     clean_text
 )
+from dotenv import load_dotenv
+from huggingface_hub import login
+
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+login(hf_token, add_to_git_credential=True)
+
 
 # LORA MODULES
 LORA_TARGET_MODULES = [
@@ -54,6 +69,7 @@ LORA_TARGET_MODULES = [
     "gate_proj", "up_proj", "down_proj",
 ]
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 # Prompt management for training and inference
@@ -166,17 +182,6 @@ def load_datasets(args):
 
     return train_ds, eval_ds
 
-
-def argument():
-    args = ArgumentParser(description='LLM Fine-Tuning')
-
-
-    return args
-
-def train():
-    args = argument()
-
-
 # Logger
 class LoggerCallback(TrainerCallback):
     def __init__(self, log_dir='log'):
@@ -229,10 +234,126 @@ class LoggerCallback(TrainerCallback):
         msg = f"best eval_loss={best:.4f}" if best else "hoàn tất"
         print(f"🏁 Training {msg}")
 
+def argument():
+    parser = ArgumentParser(description='LLM Fine-Tuning')
+
+    parser.add_argument('--base_model', type=str, default='sail/Sailor-4B-Chat')
+
+    parser.add_argument('--bpe_path', type=str, default=BPE_SAVE_PATH)
+    parser.add_argument('--processed_path', type=str, default=PROCESSED_PATH)
+    parser.add_argument('--output_dir', type=str, default='outputs')
+
+    parser.add_argument("--epochs", type=int, default=3)
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--grad_accum", type=int, default=8)
+    parser.add_argument("--lr", type=float, default=2e-4)
+    parser.add_argument("--max_seq_len", type=int, default=MAX_SEQ_LEN)
+    parser.add_argument("--warmup_ratio", type=float, default=0.03)
+    # parser.add_argument('--warmup_steps', type=int, default=1000)
+    parser.add_argument("--weight_decay", type=float, default=0.001)
+    parser.add_argument("--max_grad_norm", type=float, default=0.3)
+    parser.add_argument("--seed", type=int, default=42)
+
+    parser.add_argument("--lora_r", type=int, default=16)
+    parser.add_argument("--lora_alpha", type=int, default=32)
+    parser.add_argument("--lora_dropout", type=float, default=0.05)
+
+    parser.add_argument("--log_steps", type=int, default=10)
+    parser.add_argument("--save_steps", type=int, default=500)
+    parser.add_argument("--eval_steps", type=int, default=500)
+    parser.add_argument("--save_total_limit", type=int, default=3)
+
+    parser.add_argument("--max_samples", type=int, default=None)
+    parser.add_argument("--resume", type=str, default=None)
+
+    args = parser.parse_args()
+    return args
+
+def train():
+    args = argument()
+    set_seed(args.seed)
+
+    prompt_builder = MathPromptBuilder()
+    tokenizer = load_bpe_tokenizer(args.bpe_path)
+    bnb_config = get_bnb_config()
+
+    model = AutoModelForCausalLM.from_pretrained(
+        args.base_model,
+        quantization_config=bnb_config,
+        device_map='auto',
+        trust_remote_code=True,
+        dtype=torch.bfloat16,
+    )
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
+
+    # Trainable : 778,426,880 / 2,364,398,080  (32.923%)
+    if len(tokenizer) > model.config.vocab_size:
+        print(f"Resizing embeddings: {model.config.vocab_size:,} to {len(tokenizer):,}")
+        model.resize_token_embeddings(len(tokenizer))
+
+    model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)
+
+    print('QLoRA - r={} | alpha={} | dropout={}'.format(args.lora_r, args.lora_alpha, args.lora_dropout))
+    model = get_peft_model(model, get_lora_config(args))
+
+    print("Trainable Params")
+    print_trainable_params(model)
+
+    train_ds, eval_ds = load_datasets(args)
+
+    log_dir = os.path.join(args.output_dir, 'logs')
+    training_args = SFTConfig(
+        output_dir=args.output_dir,
+        num_train_epochs=args.epochs,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        gradient_accumulation_steps=args.grad_accum,
+        optim="paged_adamw_32bit",
+        learning_rate=args.lr,
+        weight_decay=args.weight_decay,
+        max_grad_norm=args.max_grad_norm,
+        warmup_ratio=args.warmup_ratio,
+        lr_scheduler_type="cosine",
+        fp16=False,
+        bf16=True,
+        gradient_checkpointing=True,
+        dataloader_num_workers=4,
+        logging_dir=log_dir,
+        logging_steps=args.log_steps,
+        save_strategy="steps",
+        save_steps=args.save_steps,
+        save_total_limit=args.save_total_limit,
+        eval_strategy="steps",
+        eval_steps=args.eval_steps,
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_loss",
+        report_to="tensorboard",
+        remove_unused_columns=False,
+        seed=args.seed,
+        max_length=args.max_seq_len,
+        packing=False,
+    )
+
+    collator = DataCollatorForSeq2Seq(
+        tokenizer=tokenizer,
+        model=model,
+        label_pad_token_id=100,
+        pad_to_multiple_of=8,
+        padding=True
+    )
+
+    callback = LoggerCallback(log_dir=log_dir)
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_ds,
+        eval_dataset=eval_ds,
+        processing_class=tokenizer,
+        data_collator=collator,
+        callbacks=[callback],
+    )
+
 
 if __name__ == '__main__':
-    prompt = PromptTemplate(
-        input_variables=['system', 'query', 'response'],
-        template=SYSTEM_PROMPT_VI,
-    )
-    print(prompt)
+    train()
